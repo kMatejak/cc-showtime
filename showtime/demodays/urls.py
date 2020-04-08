7896d859d2f196063fa93d4c2526b57d09f1487e
kMatejak <ksmatejak@gmail.com>
2020-04-08 21:54:49 +0200

    added page with list of demodays and detail demoday pages

--- showtime/demodays/urls.py
+++ showtime/demodays/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'demodays'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:demoday_id>/', views.detail_demoday, name='detail_demoday'),
 ]
--- showtime/demodays/views.py
+++ showtime/demodays/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import DemoDay
 
 
 def index(request):
-    return HttpResponse("This is index page")
+    demodays_list = DemoDay.objects.all()
+    context = {'demodays_list': demodays_list}
+    return render(request, 'demodays/index.html', context)
+
+
+def detail_demoday(request, demoday_id):
+    demoday = get_object_or_404(DemoDay, pk=demoday_id)
+    return render(request,
+                  'demodays/detail_demoday.html',
+                  {'demoday': demoday})
