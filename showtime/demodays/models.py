454a9f580ac2ccc9bfd441418796afb62a973a2b
kMatejak <ksmatejak@gmail.com>
2020-04-08 21:04:59 +0200

    added models DemoDay and Demo

--- showtime/demodays/models.py
+++ showtime/demodays/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class DemoDay(models.Model):
+    test_text = models.CharField(max_length=200)
+    date = models.DateTimeField('date planned')
+
+
+class Demo(models.Model):
+    demoday = models.ForeignKey(DemoDay, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    team_name = models.CharField(max_length=66)
+    student_names = models.CharField(max_length=300)
+
